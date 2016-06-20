d525180da41473f1b9c44cb3f811122db96e9f0d
antonio nilo de araujo neto <nilo.araujo@live.com>
2016-06-20 15:52:10 -0300

    tentando usar o crossfilter pra fazer grafico interativo

--- scrapper.py
+++ scrapper.py
@@ -5,19 +5,48 @@ pagina = requests.get('http://meucongressonacional.com/lavajato/empresas')
 q = html.fromstring(pagina.content)
 lista_tr = q.xpath('//*[@id="empresas_table"]//tbody//tr')
 empresas = []
+chaves_de_empresa = [
+	'ODEBRECHT',
+	'OAS',
+	'ANDRADE GUTIERREZ',
+	'GALVAO',
+	'CAMARGO CORREA',
+	'TOYO SETAL',
+	'ENGEVIX',
+	]
 
 for tr in lista_tr:
-	cnpj = int(tr[0][0].text)
+	cnpj = tr[0][0].text.strip("',/\n,/\t")
 	nome = tr[1][0].text.strip("',/\n,/\t")
+	for chave in chaves_de_empresa:
+		if chave in nome:
+			nome = chave
+
 	empresas.append((cnpj,nome))
 # for i in empresas: print i
 
+import csv
+
+arq = open('doacoes2.csv','w')
+header  = ['Partido','Empresa','Valor']
+arq = csv.DictWriter(arq,header)
+arq.writeheader()
+
 for empresa in empresas:
-	print empresa[1]
 	pagina_da_empresa = requests.get('http://meucongressonacional.com/eleicoes2014/empresa/'+str(empresa[0]))
 	root = html.fromstring(pagina_da_empresa.content)
 	lista_tr = root.xpath('//*[@id="partido_table"]/tbody//tr')
-	doacoes = []
-	for tr in lista_tr:
-		doacoes.append((tr[0].text, tr[1].text))
-	for i in doacoes: print i
+	# doacoes = []
+	if len(lista_tr) > 0:
+		for tr in lista_tr:
+			p = tr[0].text
+			valor = filter(lambda x: x != ',', tr[1].text[:-3])
+			arq.writerow({
+				'Partido' : p,
+				'Valor' : int(valor)/1000000.0,
+				'Empresa' : empresa[1]
+				})
+			# doacoes.append((tr[0].text, tr[1].text))
+	
+	# print empresa
+	# for i in doacoes: print i

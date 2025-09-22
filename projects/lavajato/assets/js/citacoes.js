8147d12591fad446582a31a8ef6c988473c9ad1c
Antonio.Neto <antonio.neto@flatiron.com>
2025-09-22 19:53:02 -0300

    Refactor whole project

---
+++ projects/lavajato/assets/js/citacoes.js
@@ -0,0 +1,31 @@
+$.get('assets/data/delacoes.csv', function(csv) {
+    console.log(csv)
+    // csv.forEach(function(d){
+    //         d.Cervero = +d.Cervero;
+    //         d.delcidio = +d.delcidio;
+    //         d.Cervero = +d.Cervero;
+    //         d.Youssef = +d.Youssef;
+    //         d['Paulo Roberto Costa'] = +d['Paulo Roberto Costa'];
+
+    //     });
+ 
+    $('#citacoes').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        data: {
+            csv: csv
+        },
+        title: {
+            text: ''
+        },
+        
+        
+        plotOptions: {
+            series: {
+                stacking: 'normal'
+            },
+        },
+    });
+});
+console.log('melao')
